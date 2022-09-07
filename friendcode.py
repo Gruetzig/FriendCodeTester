23795e308f565ff9025337c2b281796efab07b41
Gruetzig <88926852+Gruetzig@users.noreply.github.com>
2022-09-08 00:44:48 +0200

    qol changes

--- friendcode.py
+++ friendcode.py
@@ -1,6 +1,9 @@
 import sys
 import re
 import hashlib
+import os
+# remove current log and make a new file
+os.remove("log.txt")
 f = open("log.txt", 'a')
 
 # 0 filler
@@ -15,8 +18,8 @@ if len(inp) >= 12:
     print("too long")
     sys.exit()
     
-rangenum = 10**(12-len(inp)+1)-1
-print(inp)
+rangenum = 10**(12-len(inp))-1
+print(rangenum)
 
 x = "0"
 
@@ -25,18 +28,14 @@ x = "0"
 for i in range(rangenum):
     
     fc = inp + fill_zeros(x, inp)
-    fcint = int(fc)                      #blatantly stolen from https://gist.github.com/TobiX/0106edd801c6c9f276e1fa920d8b0fd8
+    fcint = int(fc)                      # next 7 lines blatantly stolen from https://gist.github.com/TobiX/0106edd801c6c9f276e1fa920d8b0fd8
     principal = fcint & 0xffffffff
     checksum = fcint >> 32
-
     sha1 = hashlib.sha1()
     sha1.update(principal.to_bytes(4, byteorder='little'))
     calcsum = sha1.digest()[0] >> 1
-
     # print(fc, fcint, principal, checksum, calcsum)
     if checksum == calcsum:
         f.write(fc)
         f.write("\n")
     x = str(int(x)+1)
-
-
